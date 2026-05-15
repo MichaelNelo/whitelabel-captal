2d19fa4d0c28825329e58085218d8724e8147b0f
Michael Nelo <michaelnelo66@outlook.com>
2026-05-15 19:57:12 -0300

    feat: Adds captal-cli and terraform

--- manifest.scm
+++ manifest.scm
@@ -3,6 +3,7 @@
              (guix download)
              (guix build-system copy)
              (guix build-system gnu)
+             (guix build-system trivial)
              (guix gexp)
              (gnu packages java)
              (gnu packages elf)
@@ -52,7 +53,7 @@
 (define-public captal-cli
   (package
    (name "captal-cli")
-   (version "0.1.1")
+   (version "1.6.0")
    (source (local-file "out/cli/assembly.dest/out.jar" "captal.jar"))
    (build-system copy-build-system)
    (inputs (list openjdk21))
@@ -77,9 +78,43 @@
    (description "CLI tool for provisioning and deploying whitelabel captive portal locations.")
    (license license:asl2.0)))
 
+(define terraform
+  (package
+   (name "terraform")
+   (version "1.14.3")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (string-append
+           "https://releases.hashicorp.com/terraform/"
+           version "/terraform_" version "_linux_amd64.zip"))
+     (sha256
+      (base32 "0kfvkrlbccdb5jlxp0ybjxsxh8qhrjifrb1j8ywnifsi49h2m2qp"))))
+   (build-system trivial-build-system)
+   (native-inputs (list unzip))
+   (arguments
+    (list
+     #:modules '((guix build utils))
+     #:builder
+     #~(begin
+         (use-modules (guix build utils))
+         (let* ((out (assoc-ref %outputs "out"))
+                (bin (string-append out "/bin"))
+                (source (assoc-ref %build-inputs "source"))
+                (unzip (string-append (assoc-ref %build-inputs "unzip") "/bin/unzip")))
+           (mkdir-p bin)
+           (invoke unzip source "-d" bin)
+           (chmod (string-append bin "/terraform") #o755)))))
+   (synopsis "Infrastructure as Code tool")
+   (description "Terraform enables infrastructure provisioning using declarative configuration files.")
+   (home-page "https://www.terraform.io/")
+   (license #f)))
+
 (packages->manifest (list openjdk21
                           sqlite
                           ffmpeg
                           coursier
                           captal-cli
+                          terraform
+                          (specification->package "awscli")
                           (specification->package "jq")))
